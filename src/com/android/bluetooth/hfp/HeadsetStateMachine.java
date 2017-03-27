c17a1ba75e26556a1c388e694ecdc6d18d66adfa
Satheesh Kumar Pallemoni <spallemo@codeaurora.org>
2017-03-27 18:16:20 +0530

    Set A2dpSuspended=true when A2dp connects while in call.
    Set A2dpSuspended=true to audio when A2dp connects while
    in ongoing call. This will let audio not request start
    till the call is active.
    
    CRs-Fixed: 2211155
    Change-Id: I51bb59d701bd39e9a522d8a083b1935b8b231eac

--- src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -3503,6 +3503,13 @@ final class HeadsetStateMachine extends StateMachine {
         }
         else
             mA2dpConnState.put(device, state);
+        //While in call A2DP connected set A2DP suspend flag to true.
+        if((getA2dpConnState() == BluetoothProfile.STATE_CONNECTED) &&
+           isInCall() && !mA2dpSuspend) {
+            Log.d(TAG, "A2DP connected while is in call suspend A2DP.");
+            mAudioManager.setParameters("A2dpSuspended=true");
+            mA2dpSuspend = true;
+        }
         Log.d(TAG, "Exit processIntentA2dpStateChanged()");
     }
 
