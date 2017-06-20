b5f3baa613c7aeebda494caec0f8aa08b6cae850
Satheesh Kumar Pallemoni <palsat@codeaurora.org>
2017-06-20 15:32:53 +0530

    Serialize connection update intents.
    Remove the invalid code which was already taken care as part of
    serializing the connection intent updates.
    
    CRs-Fixed: 2064700
    Change-Id: I8c2afaa0b024d684acb6e007ed789be35eb5dafc

--- src/com/android/bluetooth/a2dp/A2dpStateMachine.java
+++ src/com/android/bluetooth/a2dp/A2dpStateMachine.java
@@ -1728,10 +1728,6 @@ final class A2dpStateMachine extends StateMachine {
 
         Log.i(TAG,"connectoin state change " + device + " state " + newState);
 
-        if (newState == BluetoothProfile.STATE_DISCONNECTING ||
-                newState == BluetoothProfile.STATE_CONNECTING) {
-            delay = 0;
-        }
         mWakeLock.acquire();
         mIntentBroadcastHandler.sendMessageDelayed(mIntentBroadcastHandler.obtainMessage(
                                                         MSG_CONNECTION_STATE_CHANGED,
