5cc87bdf857925e7eea1fc0684c28661c8fb47cd
Ying Wu <yingwu@codeaurora.org>
2018-08-10 16:02:55 +0800

    Bluetooth: Implement API to add a bond device
    Change-Id: I039be213df06b84d3afe6da9ed7b118c4eb0dc7a

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -767,6 +767,10 @@ public class AdapterService extends Service {
         return !mCleaningUp;
     }
 
+    private boolean isValidLinkKey(String linkKey, int keyType, int pinLen) {
+        return (linkKey.isEmpty()) ? false : true;
+    }
+
     /**
      * Handlers for incoming service calls
      */
@@ -1166,6 +1170,27 @@ public class AdapterService extends Service {
             }
             return service.createBond(device, transport, oobData);
         }
+
+        @Override
+        public boolean addOutOfBandBondDevice(BluetoothDevice device, String linkKey, int linkKeyType, int pinLen) {
+            if (!Utils.checkCallerAllowManagedProfiles(mService)) {
+                Log.w(TAG, "addOutOfBandBondDevice() - Not allowed for non-active user");
+                return false;
+            }
+
+            AdapterService service = getService();
+            if (service == null) {
+                return false;
+            }
+
+            if (!service.isValidLinkKey(linkKey, linkKeyType, pinLen)) {
+                Log.w(TAG, "Invaild linkkey");
+                return false;
+            }
+
+            return service.addOutOfBandBondDevice(device, linkKey, linkKeyType, pinLen);
+        }
+
 
         @Override
         public boolean cancelBondProcess(BluetoothDevice device) {
@@ -2044,6 +2069,57 @@ public class AdapterService extends Service {
         return true;
     }
 
+    boolean addOutOfBandBondDevice(BluetoothDevice device, String linkKey, int linkKeyType, int pinLen) {
+       enforceCallingOrSelfPermission(BLUETOOTH_ADMIN_PERM,
+           "Need BLUETOOTH ADMIN permission");
+       DeviceProperties deviceProp = mRemoteDevices.getDeviceProperties(device);
+       if (deviceProp != null && deviceProp.getBondState() != BluetoothDevice.BOND_NONE) {
+           return false;
+       }
+
+       mRemoteDevices.setBondingInitiatedLocally(Utils.getByteAddress(device));
+
+       if (DBG) Log.d(TAG,"addOutOfBandBondDevice, send ADD_OOB_BOND_DEVICE");
+
+       Message msg = mBondStateMachine.obtainMessage(BondStateMachine.ADD_OOB_BOND_DEVICE);
+       msg.obj = device;
+       msg.arg1 = linkKeyType;
+       msg.arg2 = pinLen;
+
+       Bundle b = new Bundle();
+       b.putString("linkKey", linkKey);
+       msg.setData(b);
+
+       mBondStateMachine.sendMessage(msg);
+       return true;
+    }
+
+
+    void sendGetLinkKeyIntent(String linkKey, String address, boolean keyFound, int keyType){
+        Intent intent = new Intent(BluetoothDevice.ACTION_CUSTOM_ACTION_RESULT);
+        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, address);
+
+        if (keyFound) {
+            Log.d(TAG, "found linkkey " + keyFound);
+            intent.putExtra(BluetoothDevice.EXTRA_KEY_LINK_KEY, linkKey);
+            intent.putExtra(BluetoothDevice.EXTRA_KEY_LINK_KEY_TYPE, keyType);
+
+        } else {
+            Log.e(TAG, "Can not find linkkey");
+            intent.putExtra(BluetoothDevice.EXTRA_KEY_LINK_KEY, "");
+            intent.putExtra(BluetoothDevice.EXTRA_KEY_LINK_KEY_TYPE, BluetoothDevice.LKEY_TYPE_NO_LINK);
+        }
+
+        sendBroadcast(intent, AdapterService.BLUETOOTH_PERM);
+    }
+
+    void onGetLinkKey(String linkKey, byte[] remoteAddr, boolean keyFound, int keyType) {
+        String address = Utils.getAddressStringFromByte(remoteAddr);
+
+        // Broadcast intent (to app)
+        sendGetLinkKeyIntent(linkKey, address, keyFound, keyType);
+    }
+
     public boolean isQuietModeEnabled() {
         debugLog("isQuetModeEnabled() - Enabled = " + mQuietmode);
         return mQuietmode;
@@ -2910,6 +2986,9 @@ public class AdapterService extends Service {
 
     /*package*/
     native boolean createBondOutOfBandNative(byte[] address, int transport, OobData oobData);
+
+    /*package*/
+    native boolean addOutOfBandBondDeviceNative(byte[] address, byte[] linkKey, int linkKeyType, int pinLen);
 
     /*package*/
     native boolean removeBondNative(byte[] address);
